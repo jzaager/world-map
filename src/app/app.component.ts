46400ce1afb9356695cc8d4791567dc812fbf8af
Justin <jzaager@wgu.edu>
2023-07-16 21:15:03 -0400

    Connect svg paths with click listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Interactive World Map';
+
+  fetchCountryData(e: MouseEvent) {
+    console.log(e.target);
+  }
 }
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-map',
-  templateUrl: '../../../assets/world-map.svg',
+  templateUrl: './world-map.svg',
   styleUrls: ['./map.component.css'],
 })
-export class MapComponent {}
+export class MapComponent {
+  @Output() countryClick = new EventEmitter();
+
+  onClick(e: MouseEvent): void {
+    this.countryClick.emit(e);
+  }
+}
