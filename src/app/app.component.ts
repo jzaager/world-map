1ff2a6d9c3da1dd71f682937b68b0f9b1ba12506
Justin <jzaager@wgu.edu>
2023-07-16 21:40:26 -0400

    Refactor click events to limit to svg paths only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Interactive World Map';
+  paths: any;
 
+  ngOnInit(): void {
+    this.addEventsToPaths();
+  }
+
+  addEventsToPaths(): void {
+    this.paths = Array.from(document.querySelectorAll('path')).forEach((path) =>
+      path.addEventListener('click', this.fetchCountryData)
+    );
+  }
+
+  // Create service to fetch data
+  // This func will call this.service.fetchCountryData...etc
   fetchCountryData(e: MouseEvent) {
     console.log(e.target);
   }
