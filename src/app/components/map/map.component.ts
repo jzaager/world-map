715fbdfd89d13f019acf3685ba03c3d77d94a757
Justin <jzaager@wgu.edu>
2023-07-18 11:46:31 -0400

    Change name of countryClick emitter to pathClicked

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -6,12 +6,12 @@ import { Component, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent {
-  @Output() countryClick = new EventEmitter();
+  @Output() pathClicked = new EventEmitter();
 
   onClick(e: MouseEvent): void {
     const clickedElement = e.target as HTMLElement;
     if (clickedElement.tagName === 'path') {
-      this.countryClick.emit(e);
+      this.pathClicked.emit(e);
     }
   }
 }
