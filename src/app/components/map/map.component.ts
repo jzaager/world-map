f38d66274b70afb5e39c097d2a376883e6ae37d6
Justin <jzaager@wgu.edu>
2023-07-18 11:37:19 -0400

    Refactor click event handling

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -8,16 +8,10 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class MapComponent {
   @Output() countryClick = new EventEmitter();
 
-  ngOnInit(): void {
-    let paths = Array.from(document.querySelectorAll('path'));
-    paths.forEach((path) => {
-      path.addEventListener('click', (e: MouseEvent) => {
-        this.onClick(e);
-      });
-    });
-  }
-
   onClick(e: MouseEvent): void {
-    this.countryClick.emit(e);
+    const clickedElement = e.target as HTMLElement;
+    if (clickedElement.tagName === 'path') {
+      this.countryClick.emit(e);
+    }
   }
 }
