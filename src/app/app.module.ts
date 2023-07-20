7882ab7c14ab29a792832d43df0e63af4c8144ab
Justin <jzaager@wgu.edu>
2023-07-19 22:32:47 -0400

    Add routing, setting map component as homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
@@ -8,7 +9,7 @@ import { CountryInfoComponent } from './components/country-info/country-info.com
 
 @NgModule({
   declarations: [AppComponent, MapComponent, CountryInfoComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [CountryInfoComponent],
   bootstrap: [AppComponent],
 })
